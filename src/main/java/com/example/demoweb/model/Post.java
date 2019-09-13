dab883d4337fdf599ac185d14b8a8179a376b53d
hackerdmitry <hacker.dmitry@gmail.com>
2019-09-13 22:43:44 +0500

    Bootstrap

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,12 +1,20 @@
 package com.example.demoweb.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes;
+    private Date creationDate;
 
-    public Post(String text, Integer likes) {
+    public Post(String text, Date creationDate) {
         this.text = text;
-        this.likes = likes;
+        this.creationDate = creationDate;
+        likes = 0;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
     }
 
     public Integer getLikes() {
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -4,15 +4,17 @@ import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
     public List<Post> listAllPosts() {
         return Arrays.asList(
-                new Post("Где мои деньги, Лебовски?", 6472),
-                new Post("ъуъ", 2618),
-                new Post("Выйди отсюда розбiйник", 4963)
+                new Post("Где мои деньги, Лебовски?", new Date()),
+                new Post("ъуъ", new Date()),
+                new Post("Выйди отсюда розбiйник", new Date())
         );
     }
 }
